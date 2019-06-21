bc878e22ba319aa8c327d8aecefb20012317a453
ruoxi430 <ruoxi430@gmail.com>
2019-06-21 09:41:45 -0400

    update benchmark.rmd for small & large mem benchmark; add the helper function ben_hdf_mem; add data/ and output_files to .gitignore

---
+++ main/Memory/bench_hdf5_mem.sh
@@ -0,0 +1,41 @@
+#$ -l mem_free=15G,h_vmem=15G
+#$ -cwd
+#$ -m e
+#$ -M rliu38@jhu.edu
+module load conda_R/devel
+
+mode="mem"
+method="kmeans"
+size="small"
+serial="1" #if needs to paralle across B, will set to 1, 2 or 3
+
+CURRDATE="$(date +'%T')"
+FILE="csv"
+file_name="${CURRDATE}_${method}_${serial}.${FILE}"
+dir_name="${CURRDATE}_${method}_${serial}"
+mkdir /Users/April3/benchmark-hdf5-clustering/output_files/$dir_name
+#mkdir /users/rliu/benchmark-hdf5-clustering/output_files/$dir_name
+
+cores=1
+nC=(1000)
+nG=(500)
+batch=(0.001)
+center=(3)
+initializer="random"
+
+init=true
+R --slave -e "rmarkdown::render('../benchmark.Rmd', output_dir = '../../output_files/${dir_name}')" \
+--args $mode $method $size $serial $file_name $dir_name $cores $c $g $ba $k $initializer $init
+
+init=false
+
+for c in "${nC[@]}"; do 
+	for g in "${nG[@]}"; do 
+		for ba in "${batch[@]}"; do 
+			for k in "${center[@]}";do
+				R --slave -e "rmarkdown::render('../benchmark.Rmd', output_dir = '../../output_files/${dir_name}')" \
+				--args $mode $method $size $serial $file_name $dir_name $cores $c $g $ba $k $initializer $init
+			done
+		done
+	done
+done
---
+++ scripts/bench_hdf5_mem.R
@@ -0,0 +1,134 @@
+#' @title bench_hdf5_mem
+#' Perform memory profiling for the specificed clustering method.
+#' For kmeans and regular mbkmeans, data will be read into memory. For
+#' hdf5, a pointer that points to the hdf5 file on disk will be created.
+#' Then the memory of performming the method is logged and summarized. 
+#' The maximum memory usage will be returned.
+#' @param i monte carlo simulation iteration; keep this for mclapply
+#' @param dir_name the name of directory to save the .out files
+
+bench_hdf5_mem <- function(i, n_cells, 
+                       n_genes, 
+                       k_centers, 
+                       batch_size = batch_size, 
+                       num_init = num_init, 
+                       max_iters = max_iters, 
+                       init_fraction = init_fraction, 
+                       initializer = initializer, 
+                       method, size, dir_name, serial) {
+  
+  now <- format(Sys.time(), "%b%d%H%M%S")
+  out_name <- paste0(method,"_",n_cells,"_",batch,"_", now, ".out")
+  invisible(gc())
+  
+  if (size == "small"){
+    if (method == "kmeans"){
+      Rprof(filename = here("output_files",dir_name,out_name), append = FALSE, memory.profiling = TRUE)
+      mydata <- readRDS(file = here("output_files", dir_name,"sim_data.rds"))
+      stats::kmeans(mydata, centers=k_centers, iter.max = max_iters, nstart = num_init)
+      Rprof(NULL)
+    
+      profile <- summaryRprof(filename = here("output_files",dir_name,out_name), chunksize = 1000000, 
+                              memory = "tseries", diff = FALSE)
+      max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+
+      rm(mydata)
+      rm(profile)
+      invisible(gc())
+    }
+  
+    if (method == "mbkmeans"){
+      Rprof(filename = here("output_files",dir_name,out_name), append = FALSE, memory.profiling = TRUE)
+
+      mydata <- readRDS(file = here("output_files", dir_name,"sim_data.rds"))
+      mbkmeans::mini_batch(mydata, clusters = k_centers, 
+                         batch_size = batch_size, num_init = num_init, 
+                         max_iters = max_iters, init_fraction = init_fraction,
+                         initializer = initializer, calc_wcss = FALSE)
+    
+      Rprof(NULL)
+    
+      profile <- summaryRprof(filename = here("output_files",dir_name,out_name),chunksize = 1000000, 
+                              memory = "tseries", diff = FALSE)
+      max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+    
+      rm(mydata)
+      rm(profile)
+      invisible(gc())
+    }
+  
+    if (method == "hdf5"){
+      sim_data_hdf5 <- HDF5Array(file = here("output_files", dir_name,"sim_data.h5"), name = "obs")
+      Rprof(filename = here("output_files",dir_name,out_name), append = FALSE, memory.profiling = TRUE)
+      mbkmeans::mini_batch(sim_data_hdf5, clusters = k_centers, 
+                         batch_size = batch_size, num_init = num_init, 
+                         max_iters = max_iters, init_fraction = init_fraction,
+                         initializer = initializer, calc_wcss = FALSE)
+      Rprof(NULL)
+    
+      profile <- summaryRprof(filename = here("output_files",dir_name,out_name), chunksize = 1000000, 
+                              memory = "tseries", diff = FALSE)
+      max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+    
+      rm(mydata)
+      rm(profile)
+      invisible(gc())
+    }
+  }
+  
+  if (size == "large"){
+    if (method == "kmeans"){
+      Rprof(filename = here("output_files",dir_name,out_name), append = FALSE, memory.profiling = TRUE)
+      mydata <- readRDS(file = here("data", paste0("sim_data_",as.character(nC),"_", serial, ".rds")))
+      stats::kmeans(mydata, centers=k_centers, iter.max = max_iters, nstart = num_init)
+      Rprof(NULL)
+      
+      profile <- summaryRprof(filename = here("output_files",dir_name,out_name), chunksize = 1000000, 
+                              memory = "tseries", diff = FALSE)
+      max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+      
+      rm(mydata)
+      rm(profile)
+      invisible(gc())
+    }
+    
+    if (method == "mbkmeans"){
+      Rprof(filename = here("output_files",dir_name,out_name), append = FALSE, memory.profiling = TRUE)
+      mydata <- readRDS(file = here("data", paste0("sim_data_",as.character(nC),"_", serial, ".rds")))
+      mbkmeans::mini_batch(mydata, clusters = k_centers, 
+                           batch_size = batch_size, num_init = num_init, 
+                           max_iters = max_iters, init_fraction = init_fraction,
+                           initializer = initializer, calc_wcss = FALSE)
+      
+      Rprof(NULL)
+      
+      profile <- summaryRprof(filename = here("output_files",dir_name,out_name),chunksize = 1000000,
+                              memory = "tseries", diff = FALSE)
+      max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+      
+      rm(mydata)
+      rm(profile)
+      invisible(gc())
+    }
+    
+    if (method == "hdf5"){
+      Rprof(filename = here("output_files",dir_name,out_name), append = FALSE, memory.profiling = TRUE)
+      sim_data_hdf5 <- HDF5Array(file = here("data",paste0("sim_data_",as.character(nC),"_", serial, ".h5")),
+                                 name = "obs")
+      mbkmeans::mini_batch(sim_data_hdf5, clusters = k_centers, 
+                           batch_size = batch_size, num_init = num_init, 
+                           max_iters = max_iters, init_fraction = init_fraction,
+                           initializer = initializer, calc_wcss = FALSE)
+      Rprof(NULL)
+      
+      profile <- summaryRprof(filename = here("output_files",dir_name,out_name), chunksize = 1000000, 
+                              memory = "tseries", diff = FALSE)
+      max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+      
+      rm(mydata)
+      rm(profile)
+      invisible(gc())
+    }
+  }
+  return(max_mem)
+}
