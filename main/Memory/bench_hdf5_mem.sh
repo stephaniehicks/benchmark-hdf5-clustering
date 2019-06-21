c4728852010c2f9b19b0b101d94546a3acff07aa
ruoxi430 <ruoxi430@gmail.com>
2019-06-21 15:29:48 -0400

    update bench_hdf5_mem.sh

--- main/Memory/bench_hdf5_mem.sh
+++ main/Memory/bench_hdf5_mem.sh
@@ -8,13 +8,19 @@ mode="mem"
 method="kmeans"
 size="small"
 serial="1" #if needs to paralle across B, will set to 1, 2 or 3
+cores=1
+nC=(1000)
+nG=(500)
+batch=(0.001)
+center=(3)
+initializer="random"
 
 CURRDATE="$(date +'%T')"
 FILE="csv"
 file_name="${CURRDATE}_${method}_${serial}.${FILE}"
-dir_name="${CURRDATE}_${method}_${serial}"
-mkdir /Users/April3/benchmark-hdf5-clustering/output_files/$dir_name
-#mkdir /users/rliu/benchmark-hdf5-clustering/output_files/$dir_name
+dir_name="${CURRDATE}_${method}_${nC}_${batch}_${serial}"
+#mkdir /Users/April3/benchmark-hdf5-clustering/output_files/$dir_name
+mkdir /users/rliu/benchmark-hdf5-clustering/output_files/$dir_name
 
 cores=1
 nC=(1000)
@@ -23,7 +29,7 @@ batch=(0.001)
 center=(3)
 initializer="random"
 
-init=true
+init=TRUE
 R --slave -e "rmarkdown::render('../benchmark.Rmd', output_dir = '../../output_files/${dir_name}')" \
 --args $mode $method $size $serial $file_name $dir_name $cores $c $g $ba $k $initializer $init
 
