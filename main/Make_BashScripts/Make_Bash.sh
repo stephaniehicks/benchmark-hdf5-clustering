4c0a05bed55f1d9b2e98bfc703eb3ac418937e43
ruoxi430 <ruoxi430@gmail.com>
2019-07-25 15:13:18 -0400

    finish up Make_BashScripts

--- main/Make_BashScripts/Make_Bash.sh
+++ main/Make_BashScripts/Make_Bash.sh
@@ -1,6 +1,6 @@
 #Change the path to where "benchmark-hdf5-clustering" locates
 benchmark_path="/Users/April30"
-cd $benchmark_path/benchmark-hdf5-clustering/main/
+cd ${benchmark_path}/benchmark-hdf5-clustering/main/
 
 mkdir Accuracy_Local
 mkdir Memory_Local
@@ -15,9 +15,26 @@ cd ../Time
 for dirname in */ ; do
 	for filename in ${dirname}benchmark_*.txt; do
 		name=${filename##*/}
-		cat ${dirname}head_mem.txt ${filename} >> ../../Time_Local/${name%.txt}.sh
+		cat ${dirname}head_time.txt ${filename} >> ../../Time_Local/${name%.txt}.sh
 	done
 done
 
 cd ../Memory
+for dirname in */ ; do
+	for filename in ${dirname}benchmark_*.txt; do
+		for B in {1..6}; do
+			name=${filename##*/}
+			cat ${dirname}head_mem.txt >> ../../Memory_Local/${name%.txt}_${B}.sh
+			(echo ""; echo "B_name="${B}) >> ../../Memory_Local/${name%.txt}_${B}.sh
+			cat ${filename} >> ../../Memory_Local/${name%.txt}_${B}.sh
+		done
+	done
+done
+
+for B in {4..6}; do
+	rm ${benchmark_path}/benchmark-hdf5-clustering/main/Memory_Local/*_500k_${B}.sh
+	rm ${benchmark_path}/benchmark-hdf5-clustering/main/Memory_Local/*_1m_${B}.sh
+done
+
+
 
