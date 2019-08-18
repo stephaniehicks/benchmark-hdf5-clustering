43427cc80cc5794cfdc41ca7b336750448dd30b8
ruoxi430 <ruoxi430@gmail.com>
2019-08-18 15:06:56 -0400

    add new reuslts; update preprcoessing bash script

--- main/case_studies/preprocessing/TENxPBMC68k.sh
+++ main/case_studies/preprocessing/TENxPBMC68k.sh
@@ -1,8 +1,7 @@
 #$-pe local 10
-#$ -l mem_free=4G,h_vmem=4G
+#$ -l mem_free=7G,h_vmem=7G
 #$ -cwd
 #$ -m e
 #$ -M rliu38@jhu.edu
-module load conda_R/devel
 
 R -e "rmarkdown::render('TENxPBMC68k.Rmd')"
