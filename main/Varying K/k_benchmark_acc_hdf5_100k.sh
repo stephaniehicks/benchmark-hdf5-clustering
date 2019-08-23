c975c4fdc84a08a04d6ae14b705800684e4c6ba1
ruoxi430 <ruoxi430@gmail.com>
2019-08-22 20:03:58 -0400

    debug

--- main/Varying K/k_benchmark_acc_hdf5_100k.sh
+++ main/Varying K/k_benchmark_acc_hdf5_100k.sh
@@ -17,6 +17,8 @@ batch=(0.001 0.2)
 center=(2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 sim_center=15
 initializer="random"
+max_iters=100
+num_init=10
 
 CURRDATE="$(date +'%T')"
 FILE="csv"
@@ -25,7 +27,7 @@ dir_name="${CURRDATE}_${mode}_${method}_${nC}_${batch}_${B_name}"
 
 init=TRUE
 Rscript --slave ../benchmark_varying_k.R \
---args $init $mode $dir_name $file_name $method $size $B_name $cores $c $g $ba $k $initializer $B $sim_center $data_path
+--args $init $mode $dir_name $file_name $method $size $B_name $cores $c $g $ba $k $initializer $B $sim_center $data_path $max_iters $num_init
 
 init=false
 
@@ -34,7 +36,7 @@ for c in "${nC[@]}"; do
 		for ba in "${batch[@]}"; do 
 			for k in "${center[@]}";do
 				Rscript --slave ../benchmark_varying_k.R \
-				--args $init $mode $dir_name $file_name $method $size $B_name $cores $c $g $ba $k $initializer $B $sim_center $data_path
+				--args $init $mode $dir_name $file_name $method $size $B_name $cores $c $g $ba $k $initializer $B $sim_center $data_path $max_iters $num_init
 			done
 		done
 	done
