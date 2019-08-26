0188806574da7ad61b30bcf2fde77bc5c6f9d4a8
ruoxi430 <ruoxi430@gmail.com>
2019-08-25 22:42:00 -0400

    new results

---
+++ main/simulation_300k.sh
@@ -0,0 +1,17 @@
+#$ -l mem_free=12G,h_vmem=12G
+#$ -cwd
+#$ -m e
+#$ -M rliu38@jhu.edu
+
+nC=(300000)
+nG=(1000)
+sim_center=3
+data_path="/fastscratch/myscratch/rliu/Aug_data"
+
+for c in "${nC[@]}"; do 
+	for g in "${nG[@]}"; do 
+		for i in {1..3}; do
+			Rscript simulation.R --args $c $g $sim_center $data_path $i
+		done
+	done
+done
