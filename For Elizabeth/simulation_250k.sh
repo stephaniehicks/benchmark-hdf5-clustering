8911a465277121fcde871abbce659c897d5f2729
ruoxi430 <ruoxi430@gmail.com>
2019-08-28 12:06:36 -0400

    change the number of datasets

--- For Elizabeth/simulation_250k.sh
+++ For Elizabeth/simulation_250k.sh
@@ -10,7 +10,7 @@ data_path="/fastscratch/myscratch/rliu/Aug_data"
 
 for c in "${nC[@]}"; do 
 	for g in "${nG[@]}"; do 
-		for i in {33..50}; do
+		for i in {1..50}; do
 			Rscript simulation.R --args $c $g $sim_center $data_path $i
 		done
 	done
