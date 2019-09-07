3dbb909dc061cddcdd789aa39693ab7b593fba17
ruoxi430 <ruoxi430@gmail.com>
2019-09-07 15:54:12 -0400

    organize results; edit bash scripts for better work flow

---
+++ main/case_studies/bash/tenx_pbmc68k_step2-4.sh
@@ -0,0 +1,13 @@
+#$ -pe local 10
+#$ -l mem_free=4G,h_vmem=4G
+#$ -cwd
+#$ -m e
+#$ -M rliu38@jhu.edu
+module load R/3.6.1
+
+data_name="tenx_pbmc68k"
+B_name="1"
+
+Rscript --slave ../02-normalization.R --args $data_name 
+#Rscript --slave ../03-dim-reduction.R --args $data_name $B_name
+#Rscript --slave ../04-cluster_find_k.R --args $data_name $B_name
--- main/case_studies/bash/tenx_pbmc68k_time_km.sh
+++ main/case_studies/bash/tenx_pbmc68k_time_km.sh
@@ -8,8 +8,8 @@ data_name="tenx_pbmc68k"
 mode="time"
 B_name="1"
 method="kmeans"
-batch=0.01
+batch=(0.001 0.01)
 
-Rscript --slave ../01-cluster_full.R --args $data_name $mode $B_name $method $batch
-
-
+for ba in "${batch[@]}"; do
+	Rscript --slave ../01-cluster_full.R --args $data_name $mode $B_name $method $ba
+done
