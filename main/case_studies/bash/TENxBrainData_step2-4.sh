48095efc2ad6db7ad6c46d23f84274c896726796
ruoxi430 <ruoxi430@gmail.com>
2019-09-16 13:11:00 -0400

    add for brain data

---
+++ main/case_studies/bash/TENxBrainData_step2-4.sh
@@ -0,0 +1,13 @@
+#$ -pe local 10
+#$ -l mem_free=15G,h_vmem=15G
+#$ -cwd
+#$ -m e
+#$ -M rliu38@jhu.edu
+module load R/3.6.1
+
+data_name="TENxBrainData"
+B_name="1"
+
+Rscript --slave ../02-normalization.R --args $data_name $B_name
+#Rscript --slave ../03-dim-reduction.R --args $data_name $B_name
+#Rscript --slave ../04-cluster_find_k.R --args $data_name $B_name
