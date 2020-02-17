6872e89983bf0ce62b0eb5a4742383e0df8da2ce
ruoxi430 <ruoxi430@gmail.com>
2020-02-16 22:35:19 -0500

    add files for complete HDF5 geometry analysis

--- ongoing_analysis/ChunkTest/TENxBrainData/TENxBrain_chunktest.R
+++ ongoing_analysis/ChunkTest/TENxBrainData/TENxBrain_chunktest.R
@@ -9,52 +9,51 @@ suppressPackageStartupMessages(library(here))
 size <- commandArgs(trailingOnly=T)[2]
 chunk <- commandArgs(trailingOnly=T)[3]
 batch <- as.numeric(commandArgs(trailingOnly=T)[4])
+mode <- commandArgs(trailingOnly=T)[5]
 k <- 30
 
-
-if(chunk == "default"){
+if (mode == "time"){
   time.start <- proc.time()
   tenx <- loadHDF5SummarizedExperiment(here(paste0("main/case_studies/data/subset/TENxBrainData/TENxBrainData_", size), 
-                                            paste0("TENxBrainData_", size, "_preprocessed_default")))
+                                            paste0("TENxBrainData_", size, "_preprocessed_", chunk)))
   invisible(mbkmeans(counts(tenx), clusters=k, batch_size = as.integer(dim(counts(tenx))[2]*batch), 
-                     num_init=10, max_iters=100, calc_wcss = FALSE))
+                    num_init=10, max_iters=100, calc_wcss = FALSE))
   time.end <- proc.time()
   time <- time.end - time.start
-  
+
   print(time)
-  temp_table <- data.frame(observations = dim(counts(tenx))[2], genes = dim(counts(tenx))[1], batch_size = batch,  
-                           time = time[3], geometry = chunk, dimension_1 = seed(counts(tenx))@chunkdim[1], 
-                           dimension_2 = seed(counts(tenx))@chunkdim[2])
-  write.table(temp_table, file = here("ongoing_analysis/ChunkTest/TENxBrainData", paste0(chunk,".csv")), 
+  temp_table <- data.frame(observations = dim(counts(tenx))[2], genes = dim(counts(tenx))[1], batch_size = batch, 
+                          abs_batch_size =  as.integer(dim(counts(tenx))[2]*batch), 
+                          time = time[3], geometry = chunk, dimension_1 = seed(counts(tenx))@chunkdim[1], 
+                          dimension_2 = seed(counts(tenx))@chunkdim[2])
+  write.table(temp_table, file = here("ongoing_analysis/ChunkTest/TENxBrainData/Output", paste0(mode, "_", chunk,".csv")), 
               sep = ",", append = TRUE, quote = FALSE, col.names = FALSE, row.names = FALSE)
 }
 
-#if(chunk == "one"){
-#  sim_data_hdf5 <- writeHDF5Array(data, chunkdim = c(nC,1))
-#  cluster_time <- system.time(mbkmeans::mini_batch(
-#    sim_data_hdf5, clusters = 3, 
-#    batch_size = nC*batch, num_init = 10, 
-#    max_iters = 100, init_fraction = 0.1,
-#    initializer = "random", calc_wcss = FALSE))[3]
-#  
-#  temp_table <- data.frame(observations = nC, genes = nG, batch_size = batch,  
-#                           time = cluster_time, dimension = chunk, dimension_1 = nC, dimension_2 = 1)
-#  write.table(temp_table, file = file_name, sep = ",", append = TRUE, quote = FALSE,
-#              col.names = FALSE, row.names = FALSE)
-#}
-
-if(chunk == "full"){
-  sim_data_hdf5 <- HDF5Array(file = here("data",data_name), name = "obs")
-  #sim_data_hdf5 <- writeHDF5Array(data, chunkdim = c(1, nG))
-  cluster_time <- system.time(mbkmeans::mini_batch(
-    sim_data_hdf5, clusters = 3, 
-    batch_size = nC*batch, num_init = 10, 
-    max_iters = 100, init_fraction = 0.1,
-    initializer = "random", calc_wcss = FALSE))[3]
+if (mode == "mem"){
+  now <- format(Sys.time(), "%b%d%H%M%OS3")
+  out_name <- paste0(data_name, "_", run_id, "_step1_", now, "_", batch,".out")
   
-  temp_table <- data.frame(observations = nC, genes = nG, batch_size = batch,  
-                           time = cluster_time, dimension = chunk, dimension_1 = 1, dimension_2 = nG)
-  write.table(temp_table, file = file_name, sep = ",", append = TRUE, quote = FALSE,
-              col.names = FALSE, row.names = FALSE)
+  if(!file.exists(here("main/case_studies/output/Memory_output/chunk_test"))) {
+    dir.create(here("main/case_studies/output/Memory_output/chunk_test"), recursive = TRUE)}
+  
+  Rprof(filename = here("main/case_studies/output/Memory_output/chunk_test",paste0(method, out_name)), append = FALSE, memory.profiling = TRUE)
+  tenx <- loadHDF5SummarizedExperiment(here(paste0("main/case_studies/data/subset/TENxBrainData/TENxBrainData_", size), 
+                                            paste0("TENxBrainData_", size, "_preprocessed_", chunk)))
+  invisible(mbkmeans(counts(tenx), clusters=k, batch_size = as.integer(dim(counts(tenx))[2]*batch), 
+                     num_init=10, max_iters=100, calc_wcss = FALSE))
+  Rprof(NULL)
+  
+  profile <- summaryRprof(filename = here("main/case_studies/output/Memory_output/chunk_test",paste0(method, out_name)), chunksize = -1L, 
+                          memory = "tseries", diff = FALSE)
+  max_mem <- max(rowSums(profile[,1:3]))*0.00000095367432
+  
+  print(max_mem)
+  temp_table <- data.frame(observations = dim(counts(tenx))[2], genes = dim(counts(tenx))[1], batch_size = batch, 
+                           abs_batch_size =  as.integer(dim(counts(tenx))[2]*batch), 
+                           time = max_mem, geometry = chunk, dimension_1 = seed(counts(tenx))@chunkdim[1], 
+                           dimension_2 = seed(counts(tenx))@chunkdim[2])
+  write.table(temp_table, file = here("ongoing_analysis/ChunkTest/TENxBrainData/Output", paste0(mode, "_", chunk,".csv")), 
+              sep = ",", append = TRUE, quote = FALSE, col.names = FALSE, row.names = FALSE)
 }
 
