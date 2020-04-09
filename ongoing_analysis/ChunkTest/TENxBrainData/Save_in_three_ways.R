2963e8a73aeeccc0d0ed54d63055ab39a7d76f1b
ruoxi430 <ruoxi430@gmail.com>
2020-04-09 11:22:46 -0700

    organize results; update figures for varying k

--- ongoing_analysis/ChunkTest/TENxBrainData/Save_in_three_ways.R
+++ ongoing_analysis/ChunkTest/TENxBrainData/Save_in_three_ways.R
@@ -4,7 +4,7 @@ library(SingleCellExperiment)
 
 size <- commandArgs(trailingOnly=T)[2]
 # load in the data saved in "best" way
-tenx <- loadHDF5SummarizedExperiment(here(paste0("main/case_studies/data/subset/TENxBrainData/TENxBrainData_",size), paste0("TENxBrainData_", size, "_preprocessed")))
+tenx <- loadHDF5SummarizedExperiment(here(paste0("main/case_studies/data/subset/TENxBrainData/TENxBrainData_",size), paste0("TENxBrainData_", size, "_preprocessed_best")))
 
 # save in default way: "By default (i.e. when chunkdim is set to NULL), getHDF5DumpChunkDim(dim(x)) will be used."
 saveHDF5SummarizedExperiment(tenx, 
@@ -19,4 +19,9 @@ saveHDF5SummarizedExperiment(tenx,
                              chunkdim=c(1, dim(counts(tenx))[2]), 
                              level=NULL, verbose=FALSE)
 
-
+# save in single chunk
+saveHDF5SummarizedExperiment(tenx, 
+                             dir = here(paste0("main/case_studies/data/subset/TENxBrainData/TENxBrainData_",size), paste0("TENxBrainData_", size, "_preprocessed_singleChunk")),
+                             prefix="", replace=TRUE, 
+                             chunkdim=c(dim(counts(tenx))[1], dim(counts(tenx))[2]), 
+                             level=NULL, verbose=FALSE)
