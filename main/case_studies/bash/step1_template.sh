74907b013df09bbf06ca207a7023ef32f1fdab31
Davide Risso <risso.davide@gmail.com>
2020-05-04 09:34:53 +0200

    B=10 for figure 1

--- main/case_studies/bash/step1_template.sh
+++ main/case_studies/bash/step1_template.sh
@@ -1,12 +1,12 @@
-B_name="1"
-
 if [ $method = "kmeans" ]
 then
     batch=(0.01)
 fi
 
 for ba in "${batch[@]}"; do
-    Rscript --slave ../../01-cluster_full.R --args $data_name $mode $B_name $method $ba $run_id
+    for B_name in {1..9}; do
+	Rscript --slave ../../01-cluster_full.R --args $data_name $mode $B_name $method $ba $run_id
+    done
 done
 
 
