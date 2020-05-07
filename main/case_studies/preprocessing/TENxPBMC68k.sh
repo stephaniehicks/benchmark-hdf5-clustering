02b318a7effcd717b9054e0568dd5cc23780d6d6
Davide Risso <risso.davide@gmail.com>
2020-05-07 15:27:19 +0200

    Remove unused datasets

--- main/case_studies/preprocessing/TENxPBMC68k.sh
+++
@@ -1,9 +0,0 @@
-#$-pe local 6
-#$ -l mem_free=7G,h_vmem=7G
-#$ -cwd
-#$ -m e
-#$ -M shicks19@jhu.edu
-module load conda_R/3.6
-module load pandoc/2.7.3
-
-R -e "rmarkdown::render('TENxPBMC68k.Rmd')"
--- main/case_studies/preprocessing/hca_bonemarrow.sh
+++
@@ -1,9 +0,0 @@
-#$ -pe local 10
-#$ -l mem_free=8G,h_vmem=8G
-#$ -cwd
-#$ -m e
-#$ -M shicks19@jhu.edu
-module load conda_R/3.6
-module load pandoc/2.7.3
-
-R -e "rmarkdown::render('hca_bonemarrow.Rmd')"
