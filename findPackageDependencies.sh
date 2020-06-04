20e188922914b3e2ac4e4f6066a2d9f1a459313d
Elizabeth on SCF <epurdom@stat.berkeley.edu>
2020-06-04 14:57:29 -0700

    update findPackageDependencies.sh

--- findPackageDependencies.sh
+++ findPackageDependencies.sh
@@ -3,7 +3,8 @@ grep -hor --include='*.Rmd' 'library([^)]*)' * | sort -u > installLibraries.txt
 grep -hor --include='*.R' 'library([^)]*)' * | sort -u >> installLibraries.txt
 grep -hor --include='*.Rmd' 'require([^)]*)' * | sort -u >> installLibraries.txt
 grep -hor --include='*.R' 'require([^)]*)' * | sort -u >> installLibraries.txt
-sed -i 's/library(/\"/' installLibraries.txt
-sed -i 's/)/\",/' installLibraries.txt
-sed -i '1s/^/BiocManager::install(c(/' installLibraries.txt
-sed -i '$ s/,$/),ask = FALSE)/' installLibraries.txt
+mv installLibraries.txt installLibraries.R
+sed -i 's/library(/\"/' installLibraries.R
+sed -i 's/)/\",/' installLibraries.R
+sed -i '1s/^/BiocManager::install(c(/' installLibraries.R
+sed -i '$ s/,$/),ask = FALSE)/' installLibraries.R
