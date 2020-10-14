8a45c8bf5ba997978dfa174eaeb40a6dae5e452f
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2020-10-14 13:35:09 -0700

    change so not do extra B for memory

--- main/case_studies/bash/step1_template.sh
+++ main/case_studies/bash/step1_template.sh
@@ -3,9 +3,16 @@ then
     batch=(0.01)
 fi
 
+if [ $mode = "mem" ]
+then
+    MAX=1
+else
+    MAX=9
+fi
+
 for ba in "${batch[@]}"; do
-    for B_name in {1..9}; do
-	Rscript --slave ../../01-cluster_full.R --args $data_name $mode $B_name $method $ba $run_id
+    for B_name in $(seq 1 $MAX); do
+    	Rscript --slave ../../01-cluster_full.R --args $data_name $mode $B_name $method $ba $run_id
     done
 done
 
